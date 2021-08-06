836ae0c0aeaff3a90d0982e5e3d5beea1cdd71cd
Warren Fitzpatrick <wfitzpatrick@dunelm.com>
2021-08-06 11:33:54 +0100

    Added types for utils window.ee

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,1 +1,10 @@
+declare global {
+  interface Window {
+    ee: {
+      emit: (eventName: string, args: unknown) => void
+      on: (eventName: string, func: (data: unknown) => void) => void
+    }
+  }
+}
+
 export { updatePage } from "./src/updatePage"
